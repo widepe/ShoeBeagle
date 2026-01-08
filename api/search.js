62a4338482c1caab44bfc54f3b564e64caa5e626
widepe <widepe@gmail.com>
2026-01-07 16:22:55 -0800

    Update search.js

--- api/search.js
+++ api/search.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     }
 
     // Load deals from Vercel Blob Storage
-    const blobUrl = 'https://v3gjlrmpc76mymfc.public.blob.vercel-storage.com/deals-QOlmqZ4ZxpJEmABPxIdSpEjRRtTUiZ.json';
+    const blobUrl = 'https://v3gjlrmpc76mymfc.public.blob.vercel-storage.com/deals-YW1WRqaQUIRHEBUMvzkEkFJM5b3csO.json';
     let dealsData;
     
     try {
