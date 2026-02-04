9dbdb0838e785dff154a59396b8ca5bbd9ddb54a
widepe <widepe@gmail.com>
2026-02-03 18:25:37 -0800

    Delete public/js/brandModels.js

--- public/js/brandModels.js
+++
@@ -1,6 +0,0 @@
-window.brandModels = {
-  "361°": ["Spire","Fury","Strata","Flame"],
-  "Adidas": ["Adizero Adios","Adizero Boston","Adizero Takumi Sen","Supernova","Ultraboost Light"],
-  ...
-};
-
