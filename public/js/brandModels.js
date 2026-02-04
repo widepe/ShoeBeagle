078b338312e7435aea68c08fada0e6ee1481d7ef
widepe <widepe@gmail.com>
2026-02-03 18:23:51 -0800

    Update brandModels.js

--- public/js/brandModels.js
+++ public/js/brandModels.js
@@ -1,1 +1,6 @@
-c
+window.brandModels = {
+  "361°": ["Spire","Fury","Strata","Flame"],
+  "Adidas": ["Adizero Adios","Adizero Boston","Adizero Takumi Sen","Supernova","Ultraboost Light"],
+  ...
+};
+
