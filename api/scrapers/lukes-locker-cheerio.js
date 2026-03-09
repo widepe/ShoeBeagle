f31f5e4c611d629f77d978d95874008395f7a8a9
widepe <widepe@gmail.com>
2026-03-09 14:14:32 -0700

    Update lukes-locker-cheerio.js

--- api/scrapers/lukes-locker-cheerio.js
+++ api/scrapers/lukes-locker-cheerio.js
@@ -326,10 +326,10 @@ export default async function handler(req, res) {
   }
 
   // CRON_SECRET
-  // const auth = req.headers.authorization;
-  // if (process.env.CRON_SECRET && auth !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return res.status(401).json({ success: false, error: "Unauthorized" });
-  // }
+   const auth = req.headers.authorization;
+   if (process.env.CRON_SECRET && auth !== `Bearer ${process.env.CRON_SECRET}`) {
+     return res.status(401).json({ success: false, error: "Unauthorized" });
+   }
 
   const start = Date.now();
   const timestamp = nowIso();
