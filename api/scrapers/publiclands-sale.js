b434d35f638cd468da2eb482bbc799f18d1c15a4
widepe <widepe@gmail.com>
2026-03-10 20:43:08 -0700

    Update publiclands-sale.js

--- api/scrapers/publiclands-sale.js
+++ api/scrapers/publiclands-sale.js
@@ -1,12 +1,14 @@
 // /api/scrapers/publiclands-sale.js
 
+import FirecrawlApp from "@mendable/firecrawl-js";
+
 export const config = { maxDuration: 60 };
 
 export default async function handler(req, res) {
   try {
     return res.status(200).json({
       success: true,
-      message: "publiclands-sale route is alive"
+      firecrawlType: typeof FirecrawlApp
     });
   } catch (err) {
     return res.status(500).json({
