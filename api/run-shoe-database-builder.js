fe2bc4d6a9f748a52a5f4541fc575c845e86a55c
widepe <widepe@gmail.com>
2026-04-01 20:15:38 -0700

    Update run-shoe-database-builder.js

--- api/run-shoe-database-builder.js
+++ api/run-shoe-database-builder.js
@@ -1,5 +1,3 @@
-import { runShoeResearchJob } from "../shoe_database_builder/runShoeResearchJob.js";
-
 export default async function handler(req, res) {
   try {
     const limitRaw = req.query?.limit;
